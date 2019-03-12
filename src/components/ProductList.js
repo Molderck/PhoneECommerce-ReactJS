84fcb5d459b6efb62f40b3138a58c5d56bf495ea
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-03-11 21:55:45 -0500

    Setting up Context API for ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
+import {storeProducts} from "../data";
+import {ProductConsumer} from "../Context";
 
 export default class ProductList extends Component {
   state={
-    products: []
+    products: storeProducts
   }
   render() {
     return (
       <React.Fragment>
         <div className="py-5">
           <div className="container">
+            <Title name="Our" title="Product"/>
             <div className="row">
-              <Title name="Our" title="Product"/>
+              <ProductConsumer>
+                {value => {
+                  return <h1>{value}</h1>
+                }}
+              </ProductConsumer>
             </div>
           </div>
         </div>
