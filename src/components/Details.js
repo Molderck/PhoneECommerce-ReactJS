35aee785005fafb7de0a9570474041f1707f6716
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-03-18 22:54:14 -0500

    Added product detail title, price, company and info

--- src/components/Details.js
+++ src/components/Details.js
@@ -21,8 +21,23 @@ export default class Details extends Component {
                   <img src={img} className="img-fluid" alt="Product"/>
                 </div>
                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                  
-                  </div>
+                  <h2>Model: {title}</h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    Made by: <span className="text-uppercase">{company}</span>
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>
+                      Price: <span>$</span>
+                      {price}
+                    </strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    Some info about product:
+                  </p>  
+                  <p className="text-muted lead">
+                  {info}
+                  </p>
+                </div>
               </div>
             </div>
           )
