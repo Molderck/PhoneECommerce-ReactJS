d5872ddeedf6a2869ed8b43974ec4b572f662f45
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-03-19 22:22:22 -0500

    Populated modal with needed info about product added to the cart

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -17,10 +17,21 @@ export default class Modal extends Component {
                 return (<ModalContainer>
                     <div className="container">
                     <div className="row">
-                        <div id="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
-                            <h5>
-                                Item added to the Cart
-                            </h5>
+                        <div id="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5">
+                            <h5>Item added to the Cart </h5>
+                            <img src={img} className="img-fluid" alt="Product"/>
+                            <h5>{title}</h5>
+                            <h5 className="text-muted">Price: $ {price}</h5>
+                            <Link to="/">
+                                <ButtonContainer onClick={() => closeModal()}>
+                                    Store
+                                </ButtonContainer>
+                            </Link>
+                            <Link to="/cart">
+                                <ButtonContainer cart onClick={() => closeModal()}>
+                                    Go to Cart
+                                </ButtonContainer>
+                            </Link>    
                         </div>
                     </div>
                     </div>
