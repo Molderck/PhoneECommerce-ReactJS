e0e4cb95f4b6e04245d56c66f06800867d98c5aa
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-03-20 21:36:36 -0500

    Imported and used Cart Columns Component in Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Title from "../Title";
-
+import CartColumns from "./CartColumns";
 
 export default class Cart extends Component {
   render() {
     return (
       <section>
         <Title name="Your" title="Cart"/>
+        <CartColumns>
+          
+        </CartColumns>
       </section>
     );
   }
