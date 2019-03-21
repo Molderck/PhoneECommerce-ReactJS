dc849d5698f2e7489841aeeb78c3357d5250cadb
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-03-20 21:26:41 -0500

    Initial setup for Cart Component - Displaying only title

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Title from "../Title";
+
 
 export default class Cart extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from Cart</h3>
-      </div>
+      <section>
+        <Title name="Your" title="Cart"/>
+      </section>
     );
   }
 }
